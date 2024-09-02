469bf9a5d5a93929aeb12c11e2c124542a457132
Jaka-Github <h1101231033@student.untan.ac.id>
2024-09-03 03:08:01 +0700

    Praktikum 4B - Membuat Atribut

--- ketiga.php
+++ ketiga.php
@@ -3,22 +3,34 @@
 class Lingkaran
 {
     const PHI = 3.14;
+    public $jari_jari;
 
-    public function luas($jari_jari) : float {
-        return self::PHI * $jari_jari * $jari_jari;
+    public function __construct($jari_jari) {
+        $this->jari_jari = $jari_jari;
     }
 
-    public function keliling($jari_jari) : float {
-        return 2 * self::PHI * $jari_jari;
+    public function luas() : float {
+        return self::PHI*$this->jari_jari*$this->jari_jari;
+    }
+
+    public function keliling() : float {
+        return 2*self::PHI*$this->jari_jari;
     }
 }
+
 
 class Bola
 {
     const PHI = 3.14;
 
-    public function volume($jari_jari) : float {
-        return (4/3) * self::PHI * pow($jari_jari, 3);
+    public $jari_jari;
+
+    public function __construct($jari_jari) {
+        $this->jari_jari = $jari_jari;
+    }
+
+    public function volume() : float {
+        return 4/3 * self::PHI * pow($this->jari_jari, 3);
     }
 }
 
@@ -26,8 +38,17 @@ class Tabung
 {
     const PHI = 3.14;
 
-    public function volume($jari_jari, $tinggi) : float {
-        return self::PHI * pow($jari_jari, 2) * $tinggi;
+    public $jari_jari;
+    public $tinggi;
+
+    public function __construct($jari_jari, $tinggi) {
+        $this->jari_jari = $jari_jari;
+        $this->tinggi = $tinggi;
+    }
+
+
+    public function volume() : float {
+        return self::PHI * pow($this->jari_jari, 2) * $this->tinggi;
     }
 }
 
@@ -35,15 +56,33 @@ class Kerucut
 {
     const PHI = 3.14;
 
-    public $volume;
+    public $jari_jari;
+    public $tinggi;
 
-    public function volume($jari_jari, $tinggi) : float {
-        $this->volume = (1/3) * self::PHI * pow($jari_jari, 2) * $tinggi;
-        return $this->volume;
+    public function __construct($jari_jari, $tinggi) {
+        $this->jari_jari = $jari_jari;
+        $this->tinggi = $tinggi;
+    }
+
+
+    public function volume() : float {
+        return self::PHI * pow($this->jari_jari, 2) * $this->tinggi;
+        
     }
 }
 
-$nasi_tumpeng = new Kerucut();
-echo $nasi_tumpeng -> volume(4, 10) . "cm";
+$nasi_tumpeng = new Lingkaran(4);
+echo  "LuasLingkaran = " . $nasi_tumpeng-> luas() . " cm\n";
+echo  "KelilingLingkaran = " . $nasi_tumpeng->keliling() . " cm\n\n";
+
+$nasi_tumpeng2 = new Bola(4);
+echo  "VolumeBola = " . $nasi_tumpeng2-> volume() . " cm\n\n";
+
+$nasi_tumpeng3 = new Tabung(4, 10);
+echo "VolumeTabung = " . $nasi_tumpeng3-> volume() . " cm\n\n";
+
+$nasi_tumpeng4 = new Kerucut(4, 10);
+echo "VolumeKerucut = " . $nasi_tumpeng4->volume() . " cm\n\n";
+
 
 ?>
