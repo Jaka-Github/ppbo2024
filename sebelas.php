6cf64b2ca295686bd5dd9011354bbd54cc8e4805
Jaka-Github <h1101231033@student.untan.ac.id>
2024-09-20 14:34:53 +0700

    Praktikum 6A - Menyusun Ulang Namespace dan Struktur Direktori

--- sebelas.php
+++ sebelas.php
@@ -3,7 +3,7 @@
 
 require_once 'vendor/autoload.php';
 
-use App\Admin\Dosen;
+use App\Model\Akademik\Dosen;
 $dian = new Dosen( "0013118405", "198411132015041001", " Dian Prawira", "62111111", " Jln Purnama");
 $nama = "Dian Prawira";
 $dian->mengajar($nama);
